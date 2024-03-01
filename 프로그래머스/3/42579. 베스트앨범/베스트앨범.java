d8d754fa5ce362f0d9be289ea3226ea476b7ad59
YoungH66 <161566267+YoungH66@users.noreply.github.com>
2024-03-01 20:54:12 +0900

    [level 3] Title: 베스트앨범, Time: 0.87 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,68 @@
+// 1. 수록된 노래의 총 재생 횟수가 가장 많은 장르 우선 수록?
+// 2. 해당 장르의 가장 많이 재생된 노래 먼저 수록?
+// 3. 만약 재생 횟수가 같다면 고유 번호가 낮은 노래를 먼저 수록
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    
+    static class Music{
+        String genre;
+        int play;
+        int idx;
+
+        public Music(String genre, int play, int idx) {
+            this.genre = genre;
+            this.play = play;
+            this.idx = idx;
+        }
+    }
+    
+    public int[] solution(String[] genres, int[] plays) {
+         HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0; i<genres.length; i++){
+            map.put(genres[i], map.getOrDefault(genres[i], 0)+plays[i]);
+        }
+
+        // 1. 장르 선정
+        ArrayList<String> genres_ordered = new ArrayList<>();
+        while(map.size()!=0){
+            int max = -1;
+            String max_key = "";
+            for(String key : map.keySet()){
+                int tmp_cnt = map.get(key);
+                if(tmp_cnt>max){
+                    max = tmp_cnt;
+                    max_key = key;
+                }
+            }
+            genres_ordered.add(max_key);
+            map.remove(max_key);
+        }
+		
+        // 2. 장르 내 노래 선정
+        ArrayList<Music> result = new ArrayList<>();
+        for(String gern : genres_ordered){
+            ArrayList<Music> list = new ArrayList<>();
+            for(int i=0; i<genres.length; i++){
+                if(genres[i].equals(gern)){
+                    list.add(new Music(gern, plays[i], i));
+                }
+            }
+            Collections.sort(list, (o1, o2) -> o2.play - o1.play); // 내림차순 소팅
+            result.add(list.get(0)); 	// 1개는 무조건 수록
+            if(list.size()!=1){ 	// 더 수록할 곡이 있으면(==장르 내의 노래가 1개보다 많으면) 수록
+                result.add(list.get(1));
+            }
+        }
+        
+        // print result
+        int[] answer = new int[result.size()];
+        for(int i=0; i<result.size(); i++){
+            answer[i] = result.get(i).idx;
+        }
+        return answer;
+    }
+}
