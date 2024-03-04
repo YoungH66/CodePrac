f8c9bebf4d316643cc324eb00b6c20e47162ca60
YoungH66 <161566267+YoungH66@users.noreply.github.com>
2024-03-04 21:16:31 +0900

    [level 2] Title: H-Index, Time: 0.39 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+// 로직 이해 필요
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+
+        int max = 0;
+        for(int i = citations.length-1; i > -1; i--){
+            int min = (int)Math.min(citations[i], citations.length - i);
+            if(max < min) max = min;
+        }
+
+        return max;
+    }
+}
