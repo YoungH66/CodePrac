0c53d44e0df3ea019f86fa93dc6e597f27f2d6a1
YoungH66 <161566267+YoungH66@users.noreply.github.com>
2024-03-07 21:23:24 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
 
 ### 성능 요약
 
-메모리: 65.8 MB, 시간: 0.02 ms
+메모리: 75.6 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 07일 21:13:55
+2024년 03월 07일 21:23:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,11 @@
+//이해 못함
+
+import java.util.*;
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int sumOfTwoSides = brown / 2;
-        for(int length = sumOfTwoSides - 1; length >= sumOfTwoSides / 2 ; length--){
-            int width = sumOfTwoSides - length;
-            if((width-1) * (length-1) == yellow){
-                answer[0] = length + 1;
-                answer[1] = width + 1;
-                return answer;
-            }
-        }
+    public int[] solution(int brown, int red) {
+        int a = (brown+4)/2;
+        int b = red+2*a-4;
+        int[] answer = {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
         return answer;
     }
 }
