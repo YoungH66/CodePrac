42fb53aad65af49eddb05999638fdbace292a1f0
YoungH66 <161566267+YoungH66@users.noreply.github.com>
2024-03-11 18:05:41 +0900

    [level 2] Title: 타겟 넘버, Time: 0.41 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 0.38 ms
+메모리: 71.5 MB, 시간: 0.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 18:04:57
+2024년 03월 11일 18:05:41
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,17 @@
+// 가장 좋아요를 많이 받은 풀이
 class Solution {
-    int count = 0;
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        dfs(numbers, 0, target, 0);
-        answer = count;
-        
+        answer = dfs(numbers, 0, 0, target);
         return answer;
     }
-    public void dfs(int[] numbers, int depth, int target, int result){
-        if (depth == numbers.length){ //마지막 노드까지 진행했을 때
-            if (target == result){ //target값과 합계가 같다면
-                count++;
+    int dfs(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) {
+                return 1;
             }
-            return;
+            return 0;
         }
-        
-        int plus = result + numbers[depth]; //양수를 더한 값
-        int minus = result - numbers[depth]; //음수를 더한 값
-        
-        dfs(numbers, depth+1, target, plus);
-        dfs(numbers, depth+1, target, minus);
-        
+        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
     }
 }
